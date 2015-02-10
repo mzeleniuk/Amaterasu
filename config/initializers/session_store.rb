3163e7e5ffd5121f18681797036e2ea990014f5c
Michael Zelenyuk <zell.xp@gmail.com>
2015-02-10 15:59:35 +0200

    Edit production settings

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Amaterasu::Application.config.session_store :cookie_store, key: '_sample_app_session'
+Amaterasu::Application.config.session_store :cookie_store, key: '_Amaterasu_session'
