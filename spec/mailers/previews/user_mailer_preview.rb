df13305de1fcca313e0ccf9bdf8806c0e82e3320
Michael Zelenyuk <zell.xp@gmail.com>
2015-08-12 17:46:41 +0300

    Fix Mailer tests

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,7 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 
-  # Preview this email at
   # http://localhost:3000/rails/mailers/user_mailer/account_activation
   def account_activation
     user = User.first
@@ -9,10 +8,8 @@ class UserMailerPreview < ActionMailer::Preview
     UserMailer.account_activation(user)
   end
 
-  # Preview this email at
   # http://localhost:3000/rails/mailers/user_mailer/password_reset
   def password_reset
     UserMailer.password_reset
   end
-
 end
