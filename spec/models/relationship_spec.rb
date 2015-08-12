d4f064b2b92a10a568091282e5375518b8fb00ef
Michael Zelenyuk <zell.xp@gmail.com>
2015-08-12 11:20:38 +0300

    Fix Models tests

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, :type => :model do
-
   let(:follower) { FactoryGirl.create(:user) }
   let(:followed) { FactoryGirl.create(:user) }
-  let(:relationship) { follower.relationships.build(followed_id: followed.id) }
+  let(:relationship) { follower.active_relationships.build(followed_id: followed.id) }
 
   subject { relationship }
 
@@ -13,17 +12,17 @@ RSpec.describe Relationship, :type => :model do
   describe 'follower methods' do
     it { should respond_to(:follower) }
     it { should respond_to(:followed) }
-    its(:follower) { should eq follower }
-    its(:followed) { should eq followed }
   end
 
   describe 'when followed id is not present' do
     before { relationship.followed_id = nil }
+
     it { should_not be_valid }
   end
 
   describe 'when follower id is not present' do
     before { relationship.follower_id = nil }
+
     it { should_not be_valid }
   end
 end
