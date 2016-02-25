04488b22ee4b9389042fdc408fde412312c04413
Michael Zelenyuk <zell.xp@gmail.com>
2016-02-25 10:20:11 +0200

    Update CircleCI code coverage metrics configuration

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,6 +3,12 @@ require 'cucumber/rails'
 require 'cucumber/rspec/doubles'
 require 'simplecov'
 
+# Save to CircleCI's artifacts directory if we're on CircleCI
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
+  SimpleCov.coverage_dir(dir)
+end
+
 SimpleCov.start
 
 ActionController::Base.allow_rescue = false
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,26 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+
 require 'rubygems'
-
-require 'simplecov'
-SimpleCov.start
-
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
-
+require 'rspec/rails'
 require 'factory_girl'
 require 'capybara/rspec'
 require 'database_cleaner'
 
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rspec/rails'
+require 'simplecov'
+require 'codeclimate-test-reporter'
+
+# Save to CircleCI's artifacts directory if we're on CircleCI
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
+  SimpleCov.coverage_dir(dir)
+end
+
+SimpleCov.start
+
+CodeClimate::TestReporter.start
 
 RSpec.configure do |config|
-
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
