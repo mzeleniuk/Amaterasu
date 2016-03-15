ee466b113caea99595855b764342f2b2f12b5e3d
Michael Zelenyuk <zell.xp@gmail.com>
2016-03-15 10:21:40 +0200

    Take a screenshot when Cucumber scenario failed

--- features/support/env.rb
+++ features/support/env.rb
@@ -22,6 +22,9 @@ end
 
 DatabaseCleaner.logger = Rails.logger
 
+# Possible values are :truncation and :transaction
+# The :transaction strategy is faster, but might give you threading problems.
+# See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
 
 World FactoryGirl::Syntax::Methods
@@ -34,3 +37,20 @@ end
 Capybara.javascript_driver = :chrome
 
 Capybara.default_wait_time = 15
+
+# Take a screenshot when any of Cucumber's scenario failed.
+# http://vumanhcuongit.github.io/testing/2016/01/26/take-screenshot-when-cucumber-test-failed
+SCREENSHOT_FOLDER = File.join ENV.fetch('CIRCLE_ARTIFACTS', Rails.root.join('tmp')), 'cucumber_screenshot'
+
+AfterConfiguration do
+  FileUtils.rm_rf SCREENSHOT_FOLDER
+end
+
+After do |scenario|
+  if scenario.failed?
+    timestamp = "#{Time.zone.now.strftime('%Y-%m-%d, %H:%M:%S')}"
+    screenshot_name = "#{scenario.title} screenshot (#{timestamp}).png"
+
+    Capybara.page.save_screenshot File.join(SCREENSHOT_FOLDER, screenshot_name)
+  end
+end
