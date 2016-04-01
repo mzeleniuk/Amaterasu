58843aa32a9c42d9bca86d9325c48f4af7c643c6
Michael Zelenyuk <zell.xp@gmail.com>
2016-04-01 10:42:11 +0300

    Add progress bar to infinite scroll

--- app/assets/javascripts/infinite_scroll.js
+++ app/assets/javascripts/infinite_scroll.js
@@ -5,7 +5,11 @@ jQuery(function () {
       more_url = $('.pagination .next_page a').attr('href');
 
       if (more_url && $(window).scrollTop() > $(document).height() - $(window).height() - 500) {
-        $('.pagination').html('<p>Loading...</p>');
+        $('.pagination').html(
+          '<div class="progress progress-striped active" style="width: 50%; margin-left: 25%;">' +
+          '  <div class="bar" style="width: 100%;"></div>' +
+          '</div>'
+        );
         $.getScript(more_url);
       }
     });
